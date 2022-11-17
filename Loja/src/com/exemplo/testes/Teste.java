3a120611a9b0d3eafabed2f9bc5c24a2fb7c6999
Wilson Diniz Silva <wilson.diniz@outlook.com.br>
2022-11-16 21:06:26 -0300

    Transferencia arquivos para o repositorio

---
+++ Loja/src/com/exemplo/testes/Teste.java
@@ -0,0 +1,57 @@
+package com.exemplo.testes;
+
+import java.util.List;
+
+import com.exemplo.entidade.Cliente;
+import com.exemplo.repositorio.RepositorioCliente;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		RepositorioCliente repositorioCliente = new RepositorioCliente();
+		
+		/**
+		 * CREATE
+		 */
+//		Cliente cliente = new Cliente();
+//		
+//		cliente.setNome("Eu da Silva");
+//		cliente.setIdade(27);
+//		
+//		repositorioCliente.salvar(cliente);
+		
+		/**
+		 * READ
+		 */
+//		List<Cliente> clientes = repositorioCliente.listarTodos();
+//		
+//		for (Cliente cliente : clientes) {
+//			System.out.println(cliente.getNome());
+//		}
+		
+		/**
+		 * UPDATE
+		 */
+//		Cliente cliente = repositorioCliente.obterPorid(2);
+//		cliente.setNome("Beltrano de Souza");
+//		repositorioCliente.salvar(cliente);
+//		System.out.println(cliente.getNome());
+		
+		/**
+		 * DELETE
+		 */
+//		Cliente cliente = repositorioCliente.obterPorid(2);
+//		cliente.setNome("Beltrano de Souza");
+//		repositorioCliente.remover(cliente);
+//		System.out.println("Cliente excluído!");
+		
+		/**
+		 * SEARCH
+		 */
+//		Cliente cliente = repositorioCliente.obterPorid(2);
+//		System.out.println(cliente.getNome());
+
+	}
+
+}
